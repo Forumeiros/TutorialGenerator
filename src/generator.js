61a3d95f4d443e8598b6d0242a632f350392936e
Luiz Felipe <luizfelipefrazaogoncalves@gmail.com>
2018-03-25 01:39:52 -0300

    Inicialização do projeto.

---
+++ src/generator.js
@@ -0,0 +1,16 @@
+/*globals jQuery*/
+
+/**
+ * Tutorial generator.
+ *
+ * @author  Luiz Felipe F.
+ * @see      https://lffg.github.io
+ * @version  1.0.0
+ * @license  MIT
+ */
+
+(function($) {
+  'use strict';
+
+  console.log('Ready.');
+})(jQuery);
